9db4d3ce66f5eddcf9808c401e644cf6e3acc388
Kapechuk <y.kapechuk@gmail.com>
2015-03-04 00:34:19 +0200

    initialized masonry instance

--- public/modules/homePage/views/homePageView.js
+++ public/modules/homePage/views/homePageView.js
@@ -12,8 +12,16 @@ define([
 			template: _.template(homePageTemplate),
 			render: function () {
 				this.$el.html(this.template(App.messages));
-
+				this.msnrGridLayoutRender(Masonry);
 				return this;
+			},
+
+			msnrGridLayoutRender : function () {
+				var msnry = new Masonry( '.masonry-container', {
+					columnWidth: 200,
+					itemSelector: '.item'
+				});
+				console.log(msnry);
 			}
 		});
 		return App.HomePageView
