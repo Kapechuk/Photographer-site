d4147fe00abc1716f3eb940b7600a1fe0ab54d08
Kapechuk <y.kapechuk@gmail.com>
2015-06-17 09:34:21 +0300

    added butons to gallery

--- public/modules/gallery/views/galleryView.js
+++ public/modules/gallery/views/galleryView.js
@@ -7,9 +7,11 @@ define([
 		App.GalleryView = Backbone.View.extend({
 			tagName: 'ul',
 			className: 'lb-album',
+			selectors: {},
 			events: {
 				'click .open' : 'openFullFormatPhoto',
-				'click .lb-close' : 'closeFullFormatPhoto'
+				'click .lb-close' : 'closeFullFormatPhoto',
+				'click .gallery-arrows' : 'getPhoto'
 			},
 			initialize : function () {
 				this.addEventListeners();
@@ -17,17 +19,62 @@ define([
 			},
 			template: _.template(photoContainerTemplate),
 			render: function () {
+				this.selectors.rightArrow = $('#right-arrow');
+				this.selectors.leftArrow = $('#left-arrow');
+				this.selectors.arrows = $('.gallery-arrows');
 				this.$el.html(this.template(this.model.toJSON()));
 				this.$el.appendTo(this.parent.$el);
 			},
+			getPhoto: function (e) {
+				var button = $(e.target);
+				var photoContainer = $('.target');
+
+				switch (button.id) {
+					case "right-arrow" :
+						this.renderFullFormatPhoto(photoContainer);
+						break;
+					case "left-arrow" :
+						this.renderFullFormatPhoto(photoContainer);
+						break;
+					default :
+						break;
+				}
+			},
 			openFullFormatPhoto: function (e) {
+				var photoContainer = $(e.target).parent().siblings();
+				this.renderFullFormatPhoto(photoContainer)
+			},
+			renderFullFormatPhoto: function (photoContainer) {
 				this.closeFullFormatPhoto();
-				$(e.target).parent().siblings().addClass('target');
+				photoContainer.addClass('target');
+				this.showButtons(photoContainer);
 			},
 			closeFullFormatPhoto: function () {
 				if($('.target')){
 					$('.lb-overlay').removeClass('target');
+					this.hideButtons();
 				}
+			},
+			showButtons: function (photoContainer) {
+				var currentPhotoName = photoContainer.data('name');
+				var firstPhotoName = this.model.get('photos')[0];
+				var lastPhotoName = this.model.get('photos')[this.model.get('photos').length - 1];
+
+				switch(currentPhotoName) {
+					case firstPhotoName :
+						this.selectors.rightArrow.show();
+						this.selectors.leftArrow.hide();
+						break;
+					case lastPhotoName :
+						this.selectors.rightArrow.hide();
+						this.selectors.leftArrow.show();
+						break;
+					default :
+						this.selectors.arrows.show();
+				}
+			},
+			hideButtons: function () {
+				$('.gallery-arrows').hide();
 			},
 			fetchData: function () {
 				var that = this;
